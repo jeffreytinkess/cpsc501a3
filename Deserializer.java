b367b3998acb6c718098918b135b70fead1d47ae
jeffrey.tinkess <jeffrey.tinkess@csx.cs.ucalgary.ca>
2017-11-10 12:25:17 -0700

    refactored setting single field into seperate method to assist array setting

--- Deserializer.java
+++ Deserializer.java
@@ -100,54 +100,62 @@ public class Deserializer{
 				System.out.println("Error: non-element content used to store field information in setFields()");
 				break;
 			};
-			//Now have the element for field itself, get name of its (only) content and switch off it
-			Element referenceElement = fieldElement.getChild("value");
-			
-			
-			if (referenceElement == null){
-				//Is an object reference, we know fieldValue is an integer ID value so parse it
-				referenceElement = fieldElement.getChild("reference");
-				Text textElement = (Text) referenceElement.getContent(0);
-				String fieldValue = textElement.getText();
-				String fieldName = fieldElement.getAttributeValue("name");
-			
-				
-				Integer referenceID = Integer.parseInt(fieldValue);
-				//Then find that object in map
-				Object reference = objectMap.get(referenceID);
-				//finally, get fields name and set it
-				try{
-				obj.getClass().getDeclaredField(fieldName).set(obj,reference);
-				} catch (Exception e){System.out.println("Error setting field value");
-				break;
-				};
-			} else {
-				//Is a primitive reference
-				Text textElement = (Text) referenceElement.getContent(0);
-				String fieldValue = textElement.getText();
-				String fieldName = fieldElement.getAttributeValue("name");
-				
-				Class type;
-				Field f;
-				
-				//Get the field itself
-				try{
-				f = obj.getClass().getDeclaredField(fieldName);
-				type = f.getType();
-				System.out.println("fields type is " + type.toString());
-				} catch (Exception e){System.out.println("Error setting field value");
-				e.printStackTrace();
-				break;};
-				//Now have a reference to the class type and field, can get its value in a wrapper and set field
-				Object parsedValue = parsePrimitive(fieldValue, type);
-				try{
-					f.set(obj, parsedValue);
-					System.out.println("Successfully set value of obj " + obj.toString() + " to value " + parsedValue.toString());
-				}catch (Exception e){};
-				
-			}
+			setSingleField(obj, fieldElement);
 		}
 	}
+
+	private void setSingleField(Object obj, Element fieldElement){
+		//Now have the element for field itself, get name of its (only) content and switch off it
+		Element referenceElement = fieldElement.getChild("value");
+		
+		
+		if (referenceElement == null){
+			//Is an object reference, we know fieldValue is an integer ID value so parse it
+			referenceElement = fieldElement.getChild("reference");
+			Text textElement = (Text) referenceElement.getContent(0);
+			String fieldValue = textElement.getText();
+			String fieldName = fieldElement.getAttributeValue("name");
+		
+			
+			Integer referenceID = Integer.parseInt(fieldValue);
+			//Then find that object in map
+			Object reference = objectMap.get(referenceID);
+			//finally, get fields name and set it
+			try{
+			obj.getClass().getDeclaredField(fieldName).set(obj,reference);
+			} catch (Exception e){System.out.println("Error setting field value");
+			return;
+			};
+		} else {
+			//Is a primitive reference
+			Text textElement = (Text) referenceElement.getContent(0);
+			String fieldValue = textElement.getText();
+			String fieldName = fieldElement.getAttributeValue("name");
+			
+			Class type;
+			Field f;
+			
+			//Get the field itself
+			try{
+			f = obj.getClass().getDeclaredField(fieldName);
+			type = f.getType();
+			System.out.println("fields type is " + type.toString());
+			} catch (Exception e){System.out.println("Error setting field value");
+			e.printStackTrace();
+			return;};
+			//Now have a reference to the class type and field, can get its value in a wrapper and set field
+			Object parsedValue = parsePrimitive(fieldValue, type);
+			try{
+				f.set(obj, parsedValue);
+				System.out.println("Successfully set value of obj " + obj.toString() + " to value " + parsedValue.toString());
+			}catch (Exception e){};
+			
+		}
+	}
+	
+
+	
+
 
 	private Object parsePrimitive(String value, Class type){
 		String typeCode = type.toString();
@@ -180,7 +188,8 @@ public class Deserializer{
 		return toReturn;
 	}
 	private void setArrayElements(Element arrayElement){
-		System.out.println("DEBUG: entering arrayElements setting method");
+		int length = Integer.parseInt(arrayElement.getAttributeValue("length"));
+		
 	}
 
 
